e87825763b917e90485715f263805a84865d65d8
Danilo Aleixo <danilo_aleixo@hotmail.com>
2019-09-03 18:01:32 -0300

    Calculating house price

--- week2/ex1/ex1_multi.m
+++ week2/ex1/ex1_multi.m
@@ -104,7 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 15000; % You should change this
+sample = [1650; 3];
+sample_norm = [1; (sample - mu) / sigma];
+price = sample_norm' * theta; % You should change this
 
 
 % ============================================================
