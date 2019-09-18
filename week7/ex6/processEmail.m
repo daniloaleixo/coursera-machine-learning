8ca2a3a680ea992294019eafe189c4d91632bdf9
Danilo Aleixo <danilo_aleixo@hotmail.com>
2019-09-18 20:44:15 -0300

    ex 3 done

--- week7/ex6/processEmail.m
+++ week7/ex6/processEmail.m
@@ -99,7 +99,11 @@ while ~isempty(email_contents)
 
 
 
-
+    for i=1:length(vocabList)
+        if (strcmp(vocabList{i}, str))
+            word_indices = [word_indices ; i];
+        endif
+    endfor
 
 
 
